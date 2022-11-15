3a33a6fe62413a9b200e82434b785da94ccd1560
Robert Neijmeijer <robert98neijmeijer@gmail.com>
2022-11-15 15:39:34 +0100

    removed relationships

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def send_to_kafka(settings: dict, data: dict):
 
     producer = Producer({'bootstrap.servers': '10.152.183.181:9094'})
 
-    producer.produce(topic=topic, key=string_serializer('testkey', None), value=avro_serializer(data, SerializationContext(topic, MessageField.VALUE)))
+    producer.produce(topic=topic, key=string_serializer('testkey2', None), value=avro_serializer(data, SerializationContext(topic, MessageField.VALUE)))
 
     producer.flush()
 
