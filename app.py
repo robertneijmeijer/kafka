e110fb9162ec4edbfbbc47ed20848c095104047a
Robert Neijmeijer <robert98neijmeijer@gmail.com>
2022-12-05 18:45:18 +0100

    product owner

--- app.py
+++ app.py
@@ -158,7 +158,7 @@ def add_value(key):
 
 def find_product_owner(role):
     global YAML_DATA
-    with open("/persons.yml") as file:
+    with open('/persons.yml') as file:
         data = yaml.safe_load(file)
     for k, v in data.items():
         if v['teams'] is None:
