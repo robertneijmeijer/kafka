8b6da7ffa8ebcc6a01f3beda0445e22ea5f1f288
Robert Neijmeijer <robert98neijmeijer@gmail.com>
2022-12-20 13:08:05 +0100

    message test

--- app.py
+++ app.py
@@ -619,8 +619,7 @@ def main():
     
     validate_yaml(YAML_DATA)
     
-    # validate_names()
-    # update_product_owners()
+    validate_names()
 
     if os.getenv(KAFKA_VALIDATION_CHECK_ENV_VAR):
         validate_yaml(YAML_DATA)
